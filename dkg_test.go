3c1004802e5bcd3acd96e0ba1fa6d65683766438
Alan Lu <alanlu1023@gmail.com>
2017-12-01 17:24:03 -0600

    Refactor tests a bit

--- dkg_test.go
+++ dkg_test.go
@@ -1,5 +1,6 @@
 package dkg
 
+import "hash"
 import "testing"
 import "reflect"
 import "crypto/elliptic"
@@ -7,11 +8,16 @@ import "crypto/sha256"
 import "encoding/base64"
 import "math/big"
 
-func TestInvalidNodeConstruction(t *testing.T) {
-	curve := elliptic.P256()
-	hash := sha256.New()
+func GetValidParams(t *testing.T) (
+	curve elliptic.Curve,
+	hash hash.Hash,
+	g2x *big.Int,
+	g2y *big.Int,
+	private *big.Int,
+) {
+	curve = elliptic.P256()
+	hash = sha256.New()
 
-	var g2x, g2y *big.Int
 	var success bool
 	if g2x, success = new(big.Int).SetString("0a5d23f079fed8f443d7fa87d70849f846f941c07d77b1e1df139e8f7ff61a70", 16); !success {
 		t.Errorf("Could not initialize g2x")
@@ -20,8 +26,12 @@ func TestInvalidNodeConstruction(t *testing.T) {
 		t.Errorf("Could not initialize g2y")
 	}
 
-	private := big.NewInt(1)
+	private = big.NewInt(1)
+	return
+}
 
+func TestInvalidNodeConstruction(t *testing.T) {
+	curve, hash, g2x, g2y, private := GetValidParams(t)
 	zero := big.NewInt(0)
 
 	t.Run("Invalid g2", func(t *testing.T) {
@@ -73,19 +83,7 @@ func TestInvalidNodeConstruction(t *testing.T) {
 }
 
 func TestValidNode(t *testing.T) {
-	curve := elliptic.P256()
-	hash := sha256.New()
-
-	var g2x, g2y *big.Int
-	var success bool
-	if g2x, success = new(big.Int).SetString("0a5d23f079fed8f443d7fa87d70849f846f941c07d77b1e1df139e8f7ff61a70", 16); !success {
-		t.Errorf("Could not initialize g2x")
-	}
-	if g2y, success = new(big.Int).SetString("608e4edf904f2e1d5f54ddc708afec01fd2287fc95555139e065cbad4d5ecdba", 16); !success {
-		t.Errorf("Could not initialize g2y")
-	}
-
-	private := big.NewInt(1)
+	curve, hash, g2x, g2y, private := GetValidParams(t)
 
 	node, err := NewNode(
 		curve,
