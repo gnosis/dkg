d475c570914b05026652cea5e2ad61a59b385c92
Collin Chin <collin.chin@berkeley.edu>
2018-07-04 23:45:31 -0700

    set up crypto elliptic interface

--- ecpoint.go
+++ ecpoint.go
@@ -1,9 +1,30 @@
 package dkg
 
 import (
+	// "crypto/elliptic"
 	"math/big"
 )
 
 type ECPoint struct {
 	X, Y *big.Int
+}
+
+func (e *ECPoint) Params(a, b *big.Int) {
+
+}
+
+func (e *ECPoint) IsOnCurve(a, b *big.Int) {
+
+}
+
+func (e *ECPoint) Add(a, b *big.Int) {
+
+}
+
+func (e *ECPoint) ScalarMult(a, b *big.Int) {
+
+}
+
+func (e *ECPoint) ScalarBaseMult(a, b *big.Int) {
+
 }
