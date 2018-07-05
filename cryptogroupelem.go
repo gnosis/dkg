975187bcbbd787b8ab0cc895c443d564d6502031
Collin Chin <collin.chin@berkeley.edu>
2018-07-04 23:53:20 -0700

    proper return types for interface

--- cryptogroupelem.go
+++ cryptogroupelem.go
@@ -6,8 +6,19 @@ import (
 
 // CryptoGroupElem - Interface for dkg protocol use with multiple curves + bilinear pairings
 type CryptoGroupElem interface {
-	IsOnCurve(a CryptoGroupElem) bool
-	Add(a, b CryptoGroupElem) (e CryptoGroupElem)
-	ScalarBaseMult(k big.Int) (e CryptoGroupElem)
-	ScalarMult(a CryptoGroupElem, k big.Int) (e CryptoGroupElem)
+	Params() *GroupCurveParams
+	IsOnCurve(a *CryptoGroupElem) bool
+	Add(a, b *CryptoGroupElem) (e *CryptoGroupElem)
+	ScalarBaseMult(k *big.Int) (e *CryptoGroupElem)
+	ScalarMult(a *CryptoGroupElem, k *big.Int) (e *CryptoGroupElem)
+}
+
+// GroupCurveParams - Interface struct for curve parameters
+type GroupCurveParams struct {
+	P       *big.Int // the order of the underlying field
+	N       *big.Int // the order of the base point
+	B       *big.Int // the constant of the curve equation
+	Gx, Gy  *big.Int // (x,y) of the base point
+	BitSize int      // the size of the underlying field
+	Name    string   // the canonical name of the curve
 }
--- ecpoint.go
+++ ecpoint.go
@@ -1,7 +1,7 @@
 package dkg
 
 import (
-	// "crypto/elliptic"
+	"crypto/elliptic"
 	"math/big"
 )
 
@@ -9,22 +9,22 @@ type ECPoint struct {
 	X, Y *big.Int
 }
 
-func (e *ECPoint) Params(a, b *big.Int) {
+func (e *ECPoint) Params(a, b *big.Int) *elliptic.CurveParams {
 
 }
 
-func (e *ECPoint) IsOnCurve(a, b *big.Int) {
+func (e *ECPoint) IsOnCurve(a, b *big.Int) bool {
 
 }
 
-func (e *ECPoint) Add(a, b *big.Int) {
+func (e *ECPoint) Add(a, b *big.Int) *ECPoint {
 
 }
 
-func (e *ECPoint) ScalarMult(a, b *big.Int) {
+func (e *ECPoint) ScalarMult(a, b *big.Int) *ECPoint {
 
 }
 
-func (e *ECPoint) ScalarBaseMult(a, b *big.Int) {
+func (e *ECPoint) ScalarBaseMult(a, b *big.Int) *ECPoint {
 
 }
